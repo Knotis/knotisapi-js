ec665cc98eee7265a4433f5acc2216113b96140d
Seth Denner <seth@knotis.com>
2017-10-11 12:44:56 -0700

    Fix Promise Not Returned from Password Grant
    * I accidentally broke the passwordGrant method when I refactored the auth code that both passwordGrant and refreshToken use. I forgot to return the promise from the passwordGrant method.
    * I added unit tests to verify that passwordGrant and refreshToken both returh promises and to make sure both methods are defined.
    * Added a missing semicolon in knotis.js.
    * Incremented patch revision to 1.8.1.

--- tests/unit/knotis.js
+++ tests/unit/knotis.js
@@ -8,10 +8,35 @@ define(function (require) {
 
     registerSuite({
         name: 'knotis',
-        test_isknotisdefined: function () {
+        test_isKnotisDefined: function () {
             assert.isDefined(Knotis);
 
         },
+	test_isPasswordGrantDefined: function () {
+	    var client = new Knotis();
+	    assert.isDefined(client.passwordGrant)
+
+	},
+	test_isRefreshTokenDefined: function () {
+	    var client = new Knotis();
+	    assert.isDefined(client.passwordGrant)
+
+	},
+	test_isPromiseReturnedFromPasswordGrant: function () {
+	    var client = new Knotis();
+	    var promise = client.passwordGrant();
+
+	    assert.isDefined(promise.then);
+	    
+	},
+	test_isPromiseReturnedFromRefreshToken: function () {
+	    var client = new Knotis();
+	    var promise = client.refreshToken();
+
+	    assert.isDefined(promise.then);
+	    
+	}
+	
     });
 
 });
